533fdc4640475cdcbc0a14c9cf2c9d4c1c05645f
Konstantin Golik <nahabinolol@gmail.com>
2021-09-04 03:50:08 -0700

    Added port as env variable

--- server.js
+++ server.js
@@ -104,6 +104,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
