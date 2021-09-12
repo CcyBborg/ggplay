03f081525dbc4d417ce3bcc63f042b33d1341041
Konstantin Golik <nahabinolol@gmail.com>
2021-09-12 09:29:20 -0700

    Payment finished

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import ConfidentialPolicy from './pages/ConfidentialPolicy/ConfidentialPolicy';
 import Layout from './components/Layout/Layout';
 import Coach from './pages/Coach/Coach';
 import Dashboard from './pages/Dashboard/Dashboard';
+import SlotBooked from './pages/SlotBooked/SlotBooked';
 import TermsOfService from './pages/TermsOfService/TermsOfService';
 
 function App() {
