46680ff8d2db0afecb1da2c4815302ff580fa932
Konstantin Golik <nahabinolol@gmail.com>
2021-09-17 12:12:23 +0200

    All done

--- routes/users.js
+++ routes/users.js
@@ -82,10 +82,11 @@ router.get('/auth/discord', persistGameRank, passport.authenticate('discord'));
 
 router.get('/auth/discord/callback', passport.authenticate('discord', {
     failureRedirect: '/sign-up'
-}), (req, res) => {
-    console.log('\n\n\n\n\n!!!!!!');
-    console.log(req.session);
-    console.log('!!!!!!\n\n\n\n\n');
+}), async (req, res) => {
+    req.user.profile.game = req.session.game || '6110f38fa9258e24cce20f65';
+    req.user.profile.rank = req.session.rank;
+
+    await req.user.save();
 
     res.redirect('/coaching');
 });
