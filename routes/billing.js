68cd9b4086de1653b44a01c6c89ca64505123d2f
Konstantin Golik <nahabinolol@gmail.com>
2021-09-18 15:21:51 +0200

    All done

--- routes/billing.js
+++ routes/billing.js
@@ -8,9 +8,9 @@ const router = express.Router();
 
 function sign(body) {
     const data = Object.keys({ ...body, Password: process.env.TERMINAL_PASSWORD }).sort().filter(
-            key => key !== 'Token'
-        ).reduce(
-            (acc, cur) => acc + body[cur], '');
+        key => key !== 'Token'
+    ).reduce(
+        (acc, cur) => acc + body[cur], '');
 
     console.log(data);
 
@@ -21,34 +21,42 @@ router.post('/notify', async (req, res) => {
     try {
         console.log(sign(req.body))
         console.log(req.body.Token);
-        if (req.body.Success) {
-            if (req.body.Status === 'CONFIRMED') {
-                const order = await Order.findOne({ _id: req.body.OrderId });
-                const user = await User.findOne({ _id: order.user });
-                const slot = await LessonSlot.findOne({ _id: order.slot }).populate('lesson');
+        if (req.body.Status === 'REJECTED') {
+            const order = await Order.findOne({ _id: req.body.OrderId });
 
-                user.slots.push(slot._id)
-                slot.user = user._id;
-
-                slot.channel = `${user.nickname}-${String(slot._id).slice(0, 4)}`;
-                slot.invite = await createChannel(slot.channel, slot.lesson.maxParticipants);
-
-                await slot.save();
-                await user.save();
-
-                order.status = 'confirmed';
-                await order.save();
-            }
-
+            order.status = 'rejected';
+            await order.save();
             res.send('OK');
         } else {
-            const order = Order.findOne({ _id: req.body.OrderId });
+            if (req.body.Success) {
+                if (req.body.Status === 'CONFIRMED') {
+                    const order = await Order.findOne({ _id: req.body.OrderId });
+                    const user = await User.findOne({ _id: order.user });
+                    const slot = await LessonSlot.findOne({ _id: order.slot }).populate('lesson');
 
-            if (order) {
-                order.status = 'failed';
+                    user.slots.push(slot._id)
+                    slot.user = user._id;
+
+                    slot.channel = `${user.nickname}-${String(slot._id).slice(0, 4)}`;
+                    slot.invite = await createChannel(slot.channel, slot.lesson.maxParticipants);
+
+                    await slot.save();
+                    await user.save();
+
+                    order.status = 'confirmed';
+                    await order.save();
+                }
+
+                res.send('OK');
+            } else {
+                const order = Order.findOne({ _id: req.body.OrderId });
+
+                if (order) {
+                    order.status = 'failed';
+                }
+
+                res.send('ERROR');
             }
-
-            res.send('ERROR');
         }
     } catch (err) {
         console.log(err);
