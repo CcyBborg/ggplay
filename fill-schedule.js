a776a6f3451a4c4e2e83bddfb146d3fb47b5bb57
Konstantin Golik <nahabinolol@gmail.com>
2021-09-21 02:28:47 +0200

    All done

--- fill-schedule.js
+++ fill-schedule.js
@@ -1,54 +1,31 @@
 const mongoose = require('mongoose');
 const Lesson = require('./models/Lesson');
 const LessonSlot = require('./models/LessonSlot');
-const Coach = require('./models/Coach');
 require('dotenv/config');
 
 mongoose.connect(
     process.env.DB_CONNECTION,
     { useUnifiedTopology: true },
     async () => {
-        // const lesson = await Lesson.findOne({ _id: '614743af5e84796b4ce54444' });
+        const lesson = await Lesson.findOne({ _id: '61474fb44a5e52e14b2308c5' });
         const dates = [[
-            '20'
+            '21', '22', '23', '24', '26', '29', '30'
+        ], [
+            '02', '03', '04', '07', '08', '09', '10',
+            '11', '29', '30', '31'
         ]];
-        const month = ['09'];
-        const time = '16:30';
-
-        const coaches = await Coach.find({}).populate('lessons');
-        for (const c of coaches) {
-            const lesson = c.lessons[0]
-
-            const timestamp = `2021-09-20T16:30:00.123Z`;
-            let slot = LessonSlot.find({
-                timestamp,
-                lesson: lesson._id
-            });
-            await slot.remove();
-            return;
-
-            try {
-                slot = await slot.save();
-
-                lesson.slots.push(slot._id);
-                await lesson.save();
-            } catch(err) {
-                console.log(err);
-                return;
-            }
-        }
-
-        return;
+        const month = ['09', '10'];
+        const time = '15:30';
 
         for (let i = 0; i < dates.length; i++) {
             for (let j = 0; j < dates[i].length; j++) {
                 const timestamp = `2021-${month[i]}-${dates[i][j]}T${time}:00.123Z`;
-                // const slot = await LessonSlot.find({ timestamp, lesson: lesson._id });
-                // for (const s of slot) {
-                //     await s.remove();
-                // }
+            //     const slot = await LessonSlot.find({ timestamp, lesson: lesson._id });
+            //     for (const s of slot) {
+            //         await s.remove();
+            //     }
 
-               // await slot[0].remove();
+            //    await slot[0].remove();
                 let slot = new LessonSlot({
                     timestamp,
                     lesson: lesson._id
