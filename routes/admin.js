72678451071c940ff5d09017425a74873d79f635
Konstantin Golik <nahabinolol@gmail.com>
2021-09-25 19:00:56 +0200

    Finished admin setting

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,16 @@
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
-const AdminBroMongoose = require('@admin-bro/mongoose')
+const AdminBroMongoose = require('admin-bro-mongoose')
 
 const mongoose = require('mongoose');
 
-const run = async () => {
-    const mongooseDb = await mongoose.connect(process.env.DB_ADMIN_CONNECTION, { useUnifiedTopology: true })
-
-    // Passing resources by giving entire database
-    const adminBro = new AdminBro({
-        databases: [mongooseDb],
-        rootPath: '/admin'
-    })
-
-    AdminBro.registerAdapter(AdminBroMongoose);
-};
-
-run();
 
 const adminBro = new AdminBro({
-    databases: [],
+    databases: [mongoose],
     rootPath: '/admin'
-})
+});
+
+AdminBro.registerAdapter(AdminBroMongoose);
 
 const router = AdminBroExpress.buildRouter(adminBro);
 
