c2c3aecc9f235f1532df22087b96d238aa75c766
Konstantin Golik <nahabinolol@gmail.com>
2021-09-25 16:34:30 +0200

    Added db connection

--- routes/admin.js
+++ routes/admin.js
@@ -3,26 +3,20 @@ const AdminBroExpress = require('@admin-bro/express')
 
 const mongoose = require('mongoose');
 
-const adminBro = new AdminBro({
-  databases: [mongoose],
-  rootPath: '/admin',
-})
-
-const router = AdminBroExpress.buildRouter(adminBro);
-
 const run = async () => {
     const mongooseDb = await mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
 
     // Passing resources by giving entire database
     const adminBro = new AdminBro({
-      databases: [mongooseDb],
-      //... other AdminBroOptions
+        databases: [mongooseDb],
+        rootPath: '/admin'
     })
+
+    const router = AdminBroExpress.buildRouter(adminBro);
+
+    AdminBro.registerAdapter(AdminBroMongoose);
+
+    module.exports = router;
 };
 
-const AdminBro = require('admin-bro');
-const AdminBroMongoose = require('@admin-bro/mongoose');
-
-AdminBro.registerAdapter(AdminBroMongoose);
-
-module.exports = router;
+await run();
