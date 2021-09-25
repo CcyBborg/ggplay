c8bb727498bc467a6ef4530b1f02f97eb22bf69e
Konstantin Golik <nahabinolol@gmail.com>
2021-09-25 20:03:36 +0200

    All done

--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,7 @@ const Review = require('../models/Review');
 const Game = require('../models/Game');
 const Rank = require('../models/Rank');
 const Order = require('../models/Order');
+const User = require('../models/User');
 
 //Admin Bro
 AdminBro.registerAdapter(AdminBroMongoose)
