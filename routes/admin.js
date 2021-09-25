43ab795529d14723ddb9e8974b82f7171f7defc9
Konstantin Golik <nahabinolol@gmail.com>
2021-09-25 19:45:55 +0200

    All done

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,20 @@ const AdminBroMongoose = require('@admin-bro/mongoose')
 const AdminBroExpress = require('@admin-bro/express')
 
 const Lesson = require('../models/Lesson');
-
+const Coach = require('../models/Coach');
+const LessonSlot = require('../models/LessonSlot');
+const Game = require('../models/Game');
 
 //Admin Bro
 AdminBro.registerAdapter(AdminBroMongoose)
 
 const adminBro = new AdminBro({
-    resources: [Lesson],
-    rootPath: '/admin'
+    resources: [Lesson, Coach, LessonSLot, Game, Rank, Review, User, Order],
+    rootPath: '/admin',
+    branding: {
+        logo:,
+        companyName: 'GGPlay'
+    }
 })
 
 module.exports = AdminBroExpress.buildRouter(adminBro)
