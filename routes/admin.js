aa90e817bfc529c0ce8a4f16807a0a15bb68bf7f
Konstantin Golik <nahabinolol@gmail.com>
2021-09-25 16:19:41 +0200

    Added database to admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,18 @@
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
 
+const mongoose = require('mongoose');
+
 const adminBro = new AdminBro({
-  databases: [],
+  databases: [mongoose],
   rootPath: '/admin',
 })
 
-const router = AdminBroExpress.buildRouter(adminBro)
+const router = AdminBroExpress.buildRouter(adminBro);
+
+const AdminBro = require('admin-bro');
+const AdminBroMongoose = require('@admin-bro/mongoose');
+
+AdminBro.registerAdapter(AdminBroMongoose);
 
 module.exports = router;
