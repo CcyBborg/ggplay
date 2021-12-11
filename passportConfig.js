cacc4c8a71891ef190a2d817892d5624621cdf61
Konstantin Golik <nahabinolol@gmail.com>
2021-12-11 14:42:17 +0100

    google redirect uri fix

--- passportConfig.js
+++ passportConfig.js
@@ -112,7 +112,7 @@ module.exports = passport => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/users/auth/google/callback'
+        callbackURL: 'https://ggplay.ru/users/auth/google/callback'
     },
         function (req, accessToken, refreshToken, profile, done) {
             User.findOne({ google: profile.id }, function (err, user) {
